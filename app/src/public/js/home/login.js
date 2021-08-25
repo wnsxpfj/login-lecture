2cbddc35ba8de48e6a8089cce01f361c89daca19
wnsxpfj <wnsxpfj@naver.com>
2021-08-25 14:25:19 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -19,6 +19,15 @@ function login() {
        },
        body: JSON.stringify(req),
    }).then((res) => res.json())
-   .then((res) => {});
+   .then((res) => {
+       if (res.success){
+           location.href = "/";
+       }else {
+           alert(res.msg);
+       }
+   })
+   .catch((err) => {
+       console.error(new Error("로그인 중 에러 발생"));
+   });
 
 }
