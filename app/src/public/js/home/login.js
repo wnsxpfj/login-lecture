662ad809ee0443ae3dfe0f35f452566d770c7876
wnsxpfj <wnsxpfj@naver.com>
2021-08-25 10:05:50 +0900

    로그인을 요청하는 fetch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -13,6 +13,13 @@ function login() {
        id:id.value,
        psword: psword.value,
    };
-   console.log(req);
+   
+   fetch("/login",{
+       method:"POST",
+       header:{
+           "Content-Type": "application/json",
+       },
+       body: JSON.stringify(req),
+   });
 
 }
