a3d088cd337cfebf709fe3ec00298e31be1b05dc
wnsxpfj <wnsxpfj@naver.com>
2021-08-25 12:35:23 +0900

    백엔드에서 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const { login } = require("../../../routes/home/home.ctrl");
-
 const id = document.querySelector("#id"),
 psword = document.querySelector("#psword"),
 loginBtn = document.querySelector("button");
@@ -10,16 +8,17 @@ loginBtn.addEventListener("click", login);
 
 function login() {
    const req = {
-       id:id.value,
+       id: id.value,
        psword: psword.value,
    };
    
-   fetch("/login",{
-       method:"POST",
-       header:{
+   fetch("/login", {
+       method: "POST",
+       headers: {
            "Content-Type": "application/json",
        },
        body: JSON.stringify(req),
-   });
+   }).then((res) => res.json())
+   .then((res) => {});
 
 }
