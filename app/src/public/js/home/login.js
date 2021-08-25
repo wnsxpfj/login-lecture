0fa763be4e3c10bdbddf4389d71798cceebb9b23
wnsxpfj <wnsxpfj@naver.com>
2021-08-25 09:50:43 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,18 @@
 "use strict";
 
-console.log("hello");
-console.log("bye");
+const { login } = require("../../../routes/home/home.ctrl");
+
+const id = document.querySelector("#id"),
+psword = document.querySelector("#psword"),
+loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+   const req = {
+       id:id.value,
+       psword: psword.value,
+   };
+   console.log(req);
+
+}
