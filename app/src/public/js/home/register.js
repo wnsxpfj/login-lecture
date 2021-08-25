aac599a12e4724f9346dd39027222318c1b8a8b3
wnsxpfj <wnsxpfj@naver.com>
2021-08-25 16:58:22 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -1,18 +1,23 @@
 "use strict";
 
 const id = document.querySelector("#id"),
+name = document.querySelector("#name"),
 psword = document.querySelector("#psword"),
-loginBtn = document.querySelector("#button");
+confirmPsword = document.querySelector("#confirm-psword"),
+registerBtn = document.querySelector("#button");
 
-loginBtn.addEventListener("click", login);
+registerBtn.addEventListener("click", register);
 
-function login() {
+function register() {
    const req = {
        id: id.value,
+       name: name.value,
        psword: psword.value,
+       confirmPsword: confirmPsword.value,
    };
+   console.log(req);
    
-   fetch("/login", {
+   fetch("/register", {
        method: "POST",
        headers: {
            "Content-Type": "application/json",
@@ -21,13 +26,13 @@ function login() {
    }).then((res) => res.json())
    .then((res) => {
        if (res.success){
-           location.href = "/";
+           location.href = "/login";
        }else {
            alert(res.msg);
        }
    })
    .catch((err) => {
-       console.error(new Error("로그인 중 에러 발생"));
+       console.error(new Error("회원가입 중 에러 발생"));
    });
 
 }
