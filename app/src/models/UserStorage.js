931ce294e1e7ed95b8951a6cab4c73cd0042f1c3
wnsxpfj <wnsxpfj@naver.com>
2021-09-03 12:01:48 +0900

    AWS RDS와 연동 & dotenv로 환경변수 등록

---
+++ app/src/models/UserStorage.js
@@ -0,0 +1,32 @@
+"use strict";
+
+
+const db = require("../config/db");
+
+class UserStorage{
+static getUserInfo(id) {
+    return new Promise((resolve, reject) => {
+        const query = "SELECT * FROM users WHERE id = ?;";
+   db.query(query, [id], (err,data) => {
+    if(err) reject(`${err}`);
+    resolve(data[0]);
+   });
+});
+}
+
+
+static async save(userInfo) {
+    return new Promise((resolve, reject) => {
+        const query = "INSERT INTO users(id, name, psword) VALUES(?, ?, ?);";
+   db.query(query,
+     [userInfo.id, userInfo.name, userInfo.psword], (err) => {
+    if(err) reject(`${err}`);
+    resolve({succes: true});
+
+   });
+});
+    
+}
+}
+
+module.exports = UserStorage;
